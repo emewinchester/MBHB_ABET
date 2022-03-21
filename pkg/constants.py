9b51a07b94742c6c4498d297f41a7b0b459eb801
Marina <marina.delgado536@alu.uhu.es>
2022-03-22 00:55:36 +0100

    Algoritmo de BA terminado - comprobar funcionamien

--- pkg/constants.py
+++ pkg/constants.py
@@ -3,5 +3,10 @@ CERCANAS_KMS_PATH     = './00_datos/cercanas_kms.csv'
 DELTAS_5M_PATH        = './00_datos/deltas_5m.csv'
 
 MAX_CAPACITY = 220
-WALKING_WEIGHT = 3
+MIN_CAPACITY = 205
+
+# Los km extra andando penalizan el triple
 CYCLING_WEIGHT = 1
+WALKING_WEIGHT = 3*CYCLING_WEIGHT
+
+BA_TOTAL_ITERATIONS = 100
