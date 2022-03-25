9706cd3a4f544532deeab14fb3d9d67c5b53fb26
Marina <marina.delgado536@alu.uhu.es>
2022-03-25 20:26:09 +0100

    Enfriamiento sim terminado - param no ajustados

--- pkg/neighborhood.py
+++ pkg/neighborhood.py
@@ -1,4 +1,6 @@
 from itertools import permutations
+import numpy as np
+from random import shuffle
 
 
 class Neighborhood():
@@ -10,28 +12,55 @@ class Neighborhood():
         # list of pemutations
         perm = permutations(range(len(self.solution)), 2)
         self.neighbors = list(perm)
+        self.neighbors_sa =  self.neighbors.copy()
+        shuffle(self.neighbors_sa)
+   
+       
+
+
+    def _adjust_neighbor(self, neighbor, origin, destiny):
+
+        # movimiento (origen,destino)
+        # coge slots de origen y los añade a destino
+        if neighbor[origin] < self.slots:
+            neighbor[destiny] += (self.slots - neighbor[origin])
+            neighbor[origin]  = 0
+        else:
+            neighbor[destiny] += self.slots
+            neighbor[origin]  -= self.slots
+        
+        return neighbor
+
+    
+
 
     
     def get_neighbor(self):
 
         if len(self.neighbors) > 0:
+
             neighbor = self.solution.copy()
 
-            origin, destiny = self.neighbors[0]
-            self.neighbors.remove((origin,destiny))
+            o, d = self.neighbors[0]
+            self.neighbors.remove((o,d))
 
-            # movimiento (origen,destino)
-            # coge slots de origen y los añade a destino
-            if neighbor[origin] < self.slots:
-                neighbor[destiny] += (self.slots - neighbor[origin])
-                neighbor[origin]  = 0
-            else:
-                neighbor[destiny] += self.slots
-                neighbor[origin]  -= self.slots
-
-            return neighbor
+            return self._adjust_neighbor(neighbor, o,d)
 
         else:
             return None
 
-        
+
+
+    def get_neighbor_sa(self):
+        if len(self.neighbors_sa) > 0:
+
+            neighbor = self.solution.copy()
+
+            o, d = self.neighbors_sa[0]
+            self.neighbors_sa.remove((o,d))
+
+            return self._adjust_neighbor(neighbor, o, d)
+
+        else:
+            return None
+
