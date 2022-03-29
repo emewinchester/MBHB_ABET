8102a80db571df845a2c8fcf56101f18433d5b7b
Marina <marina.delgado536@alu.uhu.es>
2022-03-29 21:40:39 +0200

    solucion greedy de lista tabu hecha

--- pkg/utils.py
+++ pkg/utils.py
@@ -9,6 +9,7 @@ def generate_random_solution():
     total_gaps = np.sum(capacity)
 
     capacity = np.round(capacity * MAX_CAPACITY / total_gaps)
+    total_gaps = np.sum(capacity)
     
     if total_gaps > MAX_CAPACITY:
         greater_station = np.argmax(capacity)
@@ -26,6 +27,48 @@ def update_frequency_matrix(matrix, vector, base_values):
         matrix[row, column] +=1
 
     return matrix
+
+
+def generate_greedy_solution(matrix, base_values):
+    #####  SOLUCION GREEDY #####
+
+    solution = np.empty(matrix.shape[1])
+
+    inverse = 1/matrix
+
+   
+    total_per_column = inverse.sum(axis=0)
+
+    for column in range(len(solution)):
+
+        # normalizamos la columna
+        inverse[:,column] = inverse[:,column] / total_per_column[ column ]
+
+        num = np.random.rand()
+        suma = 0
+
+        for row in range(matrix.shape[0]):
+            suma += inverse[row,column]
+
+            if num < suma:
+                solution[column] = base_values[row]
+                break
+
+
+    # añadimos de 0 a 2 slots más a cada estacion
+    increase  = np.random.randint(0, 3, len(solution))
+    solution += increase
+
+    # normalizamos solucion para evitar que supere los 220 slots
+    total_slots = np.sum(solution)
+    solution = np.round(solution * MAX_CAPACITY / total_slots)
+    total_slots = np.sum(solution)
+
+    if total_slots > MAX_CAPACITY:
+        greater_station = np.argmax(solution)
+        solution[greater_station] -= total_slots - MAX_CAPACITY
+
+    return solution
 
 
 
